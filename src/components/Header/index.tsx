11ef2853e4f196b40b38980200fc64f4d2ca072d
Amanda Karlsson Printz <fannyamandakarlsson@gmail.com>
2023-04-14 11:21:57 +0200

    -i styled components & setting up file structure

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from "./style";
+/* ('import HeaderContainer from "./style"');
+ */
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <h1>My favorite veggies</h1>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+/* import { device } from "./../MediaQueries/";
+ */
+export const HeaderContainer = styled.header`
+  padding: 4em;
+  background: #142610;
+
+  h1 {
+    color: #fafaee;
+  }
+`;
